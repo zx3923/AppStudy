59f68dd37041be02e39b1903d7461ddd916b7431
zx3923 <seop3923@gmail.co>
2024-04-03 15:16:35 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import AuthStackNavigator from './src/navigations/stack/AuthStackNavigator';
+import RootNavigator from './src/navigations/root/RootNavigator';
+
+function App() {
+  return (
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ src/screens/AuthHomeScreen.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StackScreenProps} from '@react-navigation/stack';
+import {StyleSheet, Button, View, SafeAreaView} from 'react-native';
+import {AuthStackParamList} from '../navigations/stack/AuthStackNavigator';
+import {authNavigations} from '../constants';
+
+type AuthHomeScreenProps = StackScreenProps<
+  AuthStackParamList,
+  typeof authNavigations.AUTH_HOME
+>;
+
+function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
+  return (
+    <SafeAreaView>
+      <View>
+        <Button
+          title="로그인화면으로 이동"
+          onPress={() => navigation.navigate(authNavigations.LOGIN)}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default AuthHomeScreen;
