fe6590ca37906730cfc96f98affd17a44307c3cf
zx3923 <seop3923@gmail.co>
2024-04-04 09:50:02 +0900

    유효성검사, useForm추가

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,6 +1,11 @@
 const colors = {
-  PINK_700: '#C63B64',
   PINK_500: '#BF5C79',
+  PINK_700: '#C63B64',
+  RED_300: '#FFB4B4',
+  RED_500: '#FF5F5F',
+  GRAY_200: "#E7E7E7",
+  GRAY_500: "#8E8E8E",
+  GRAY_700: "#575757"
 }
 
 export { colors };
--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,14 +1,57 @@
-import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native';
+import InputField from '../../components/InputField';
+import CustomButton from '../../components/CustomButton';
+import useForm from '../../hooks/useForm';
+import {validateLogin} from '../../utils';
 
 function LoginScreen() {
+  const login = useForm({
+    initialValue: {email: '', password: ''},
+    validate: validateLogin,
+  });
+
+  const handleSubmit = () => {
+    console.log('values', login.values);
+  };
+
   return (
-    <View>
-      <Text>로그인 스크린</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="이메일"
+          error={login.errors.email}
+          touched={login.touched.email}
+          inputMode="email"
+          {...login.getTextInputProps('email')}
+        />
+        <InputField
+          placeholder="비밀번호"
+          error={login.errors.password}
+          touched={login.touched.password}
+          {...login.getTextInputProps('password')}
+          secureTextEntry
+        />
+      </View>
+      <CustomButton
+        label="로그인"
+        variant="filled"
+        size="large"
+        onPress={handleSubmit}
+      />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 30,
+  },
+});
 
 export default LoginScreen;
