52b2c0e7a4e6e810af4fe47a3b91f47a24a8f9af
zx3923 <seop3923@gmail.co>
2024-04-04 10:17:01 +0900

    회원가입스크린

--- src/screens/auth/SignupScreen.tsx
+++ src/screens/auth/SignupScreen.tsx
@@ -1,14 +1,54 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import InputField from '../../components/InputField';
+import useForm from '../../hooks/useForm';
+import CustomButton from '../../components/CustomButton';
+import {validateSignup} from '../../utils';
 
 function SignupScreen() {
+  const signup = useForm({
+    initialValue: {email: '', password: '', passwordConfirm: ''},
+    validate: validateSignup,
+  });
   return (
-    <View>
-      <Text>회원가입 스크린</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="이메일"
+          error={signup.errors.email}
+          touched={signup.touched.email}
+          inputMode="email"
+          {...signup.getTextInputProps('email')}
+        />
+        <InputField
+          placeholder="비밀번호"
+          error={signup.errors.password}
+          touched={signup.touched.password}
+          secureTextEntry
+          {...signup.getTextInputProps('password')}
+        />
+        <InputField
+          placeholder="비밀번호 확인"
+          error={signup.errors.passwordConfirm}
+          touched={signup.touched.passwordConfirm}
+          secureTextEntry
+          {...signup.getTextInputProps('passwordConfirm')}
+        />
+      </View>
+      <CustomButton label="회원가입" />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 30,
+  },
+});
 
 export default SignupScreen;
--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -3,8 +3,8 @@ type UserInfomation = {
   password: string;
 };
 
-function validateLogin(values: UserInfomation) {
-  const errors = {
+function validateUser(values: UserInfomation) {
+const errors = {
     email: '',
     password: '',
   };
@@ -15,8 +15,20 @@ function validateLogin(values: UserInfomation) {
   if (!(values.password.length >= 8 && values.password.length < 20)) {
     errors.password = '비밀번호는 8~20자 사이로 입력해주세요.';
   }
-
   return errors;
 }
 
-export { validateLogin };
+function validateLogin(values: UserInfomation) {
+  return validateUser(values);
+}
+
+function validateSignup(values: UserInfomation & {passwordConfirm:string}) {
+  const errors = validateUser(values);
+  const signupErrors = { ...errors, passwordConfirm: '' };
+  if (values.password !== values.passwordConfirm) {
+    signupErrors.passwordConfirm = '비밀번호가 일치하지 않습니다.';
+  }
+  return signupErrors;
+}
+
+export { validateLogin, validateSignup };
